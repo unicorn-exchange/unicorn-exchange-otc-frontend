ffcdc6a7ea32b81a255dd5ff6ae447a2aa1a9308
Julia <lolpnon101@mail.ru>
2019-09-21 15:29:51 +0300

    Added responsive design (not full), added sidebar and sidebar animation

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,9 +7,14 @@ import {Component, OnInit} from "@angular/core";
 })
 export class HeaderComponent implements OnInit {
 
+  showSideBar = false;
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  toggleSideBar() {
+    this.showSideBar = !this.showSideBar;
   }
 }
