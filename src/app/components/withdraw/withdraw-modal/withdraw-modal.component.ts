2e1de40a4bcbe85e475bcc4f7fd44aa6fc8ba333
Julia <lolpnon101@mail.ru>
2019-10-08 00:46:35 +0300

    withdraw update

--- src/app/components/withdraw/withdraw-modal/withdraw-modal.component.ts
+++ src/app/components/withdraw/withdraw-modal/withdraw-modal.component.ts
@@ -1,25 +1,75 @@
-import {Component, Input, OnInit} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
-import {Subject} from "rxjs";
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {takeUntil} from "rxjs/operators";
+import {genCtrl} from "../../../../services/utils";
+import * as yup from "yup";
+import {BaseComponent} from "../../base-component/base.component";
+
+enum withDrawFields {
+  address = "address",
+  amount = "amount",
+  password = "password",
+}
+
+const withDrawValidationScheme = {
+  [withDrawFields.address]: yup
+    .string()
+    .nullable()
+    .required(),
+  [withDrawFields.password]: yup
+    .string()
+    .nullable()
+    .required(),
+  [withDrawFields.amount]: yup
+    .number()
+    .nullable()
+    .required(),
+};
 
 @Component({
   selector: "app-withdraw-modal-content",
   templateUrl: "./withdraw-modal.component.html",
   styleUrls: ["./withdraw-modal.component.scss"]
 })
-export class WithdrawModalComponent implements OnInit {
-  @Input() name;
-  address = "0xa94ab44a1c16714dad4f732410c49a112d8d3605";
-  alertType = new Subject<string>();
+export class WithdrawModalComponent extends BaseComponent implements OnInit {
+  submitted = false;
+  formFields = withDrawFields;
+  scheme = withDrawValidationScheme;
+  form: FormGroup = this.fb.group(Object.assign(
+    genCtrl({key: this.formFields.address, scheme: this.scheme}),
+    genCtrl({key: this.formFields.amount, scheme: this.scheme}),
+    genCtrl({key: this.formFields.password, scheme: this.scheme})
+  ));
 
 
-  constructor(public activeModal: NgbActiveModal) {
+  constructor(
+    public activeModal: NgbActiveModal,
+    private fb: FormBuilder,
+  ) {
+    super();
   }
 
   ngOnInit() {
+    this.form.valueChanges
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(v => {
+        console.log(v);
+      });
   }
 
-  successCopy() {
-    this.alertType.next("success");
+  onSubmit(event, formData) {
+    this.submitted = true;
+    event.preventDefault();
+    if (this.form.invalid) {
+      return;
+    }
+
+    // this.authStore
+    //   .signIn(formData)
+    //   .then(() => this.router.navigate([ROUTES.OPEN_MARKET]))
+    //   .catch(err => {
+    //     console.log(err);
+    //   });
   }
 }
